acda460876681c573dd3839751bbcf92a86f17da
asmit487-umd <asmit487MC@gmail.com>
2026-04-27 22:39:42 -0400

    Update test.py
    fixed to correct csv oopsies

--- test.py
+++ test.py
@@ -51,7 +51,7 @@ class Employee:
     def PayEmployee(self, name, hourly_rate, min_hours, max_hours, overtime_eligibility):
         self.compensation = self.hourly_rate * self.hours_scheduled
         return self.compensation
-        employees = pd.read_csv("workweek.csv", names = ["name", "hourly rate", "minimum hours", "max hours", "overtime eligibility"])
+        employees = pd.read_csv("employees.csv", names = ["name", "hourly rate", "minimum hours", "max hours", "overtime eligibility"])
         
 
     def EmployeeOvertime(self, hours_worked):
